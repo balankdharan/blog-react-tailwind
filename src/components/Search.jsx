5a11d3bc7a56275e12b57686e50f7aecbee646bb
balankdharan <balankdharan@gmail.com>
2024-07-27 08:11:08 +0530

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,14 @@
+import banner from "../assets/images/blogBanner.png";
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="justify-center flex mt-8 flex-col">
+      <img src={banner} alt="banner" className="rounded-2xl" />
+      <div>
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
