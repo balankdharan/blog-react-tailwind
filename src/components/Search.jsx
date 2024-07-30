33e70b275acdaee5f835114ffe26ead32caedad9
balankdharan <balankdharan@gmail.com>
2024-07-30 19:19:25 +0530

    added UI

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import banner from "../assets/images/blogBanner.png";
 
 const Search = () => {
   return (
-    <div className="justify-center flex mt-8 flex-col">
+    <div className="justify-center flex mt-8 flex-col px-[70] md:px-[150px]">
       <img src={banner} alt="banner" className="rounded-2xl" />
       <div>
         <input type="text" placeholder="Search" />
